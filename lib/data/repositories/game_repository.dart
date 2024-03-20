e61d9c2e39dad140c9d43ede4b8e5bd15516d3c3
Emanuel Costa <emanuelldsc@gmail.com>
2024-03-20 18:19:43 -0300

    game_repository

--- lib/data/repositories/game_repository.dart
+++ lib/data/repositories/game_repository.dart
@@ -1,13 +1,36 @@
+import 'dart:convert';
+
+import 'package:f2p_games/data/http/exceptions.dart';
+import 'package:f2p_games/data/http/http_client.dart';
 import 'package:f2p_games/data/models/games_models.dart';
 
 abstract class IGameRepository {
   Future<List<GameModel>> getGames();
 }
 
-class GameRepository implements IGameRepository{
+class GameRepository implements IGameRepository {
+  final IHttpClient client;
+
+  GameRepository({required this.client});
+
   @override
-  Future<List<GameModel>> getGames() {
-    // TODO: implement getGames
-    throw UnimplementedError();
+  Future<List<GameModel>> getGames() async {
+    final response =
+        await client.get(url: 'https://www.freetogame.com/api/games');
+
+    if (response.statusCode == 200) {
+      final List<GameModel> games = [];
+      final body = jsonDecode(response.body);
+      body.map(() {}).toList().map((item) {
+        final GameModel game = GameModel.fromMap(item);
+        games.add(game);
+      }).toList();
+
+      return games;
+    } else if (response.statusCode == 404) {
+      throw NotFoundException('Invalid URL');
+    } else {
+      throw Exception('Impossible to load the games');
+    }
   }
 }
