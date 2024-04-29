a37a5d1602f6a8f9f9df7df3f3d9ce74f07f7f18
Emanuel Costa <emanuelldsc@gmail.com>
2024-04-29 20:19:11 -0300

    fix: favourites list

--- lib/view/pages/favourite/saved_games_page.dart
+++ lib/view/pages/favourite/saved_games_page.dart
@@ -10,10 +10,9 @@ import '../../widgets/cards/favourites_cards_widgets.dart';
 import '../../widgets/my_progress_indicador_widget.dart';
 
 class FavouritesPage extends StatefulWidget {
-  const FavouritesPage({super.key});
+  const FavouritesPage({Key? key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _FavouritesPageState createState() => _FavouritesPageState();
 }
 
@@ -25,17 +24,47 @@ class _FavouritesPageState extends State<FavouritesPage> {
       body: Container(
         padding: const EdgeInsets.all(10),
         child: StreamBuilder<QuerySnapshot>(
-            stream: FirebaseFirestore.instance
-                .collection('Users')
-                .doc(FirebaseAuth.instance.currentUser?.uid)
-                .collection('favourites')
-                .snapshots(),
-            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              //checking connection state
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(child: MyCircularProgressIndicator());
-              }
+          stream: FirebaseFirestore.instance
+              .collection('Users')
+              .doc(FirebaseAuth.instance.currentUser?.uid)
+              .collection('favourites')
+              .snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            //checking connection state
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: MyCircularProgressIndicator());
+            }
+            if (snapshot.hasData) {
               if (snapshot.data!.docs.isEmpty) {
+                return Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        LottieBuilder.network(
+                            'https://lottie.host/85b0686f-fad8-4930-a12f-704af412d345/AG04t7ZsMf.json'),
+                        const SizedBox(height: 10),
+                        const MyText(
+                          googleFont: GoogleFonts.lato,
+                          color: Colors.white,
+                          fontSize: 32,
+                          title: 'Ohh nooo!',
+                          weight: FontWeight.bold,
+                        ),
+                        const SizedBox(height: 10),
+                        const MyText(
+                          googleFont: GoogleFonts.lato,
+                          color: Colors.grey,
+                          fontSize: 20,
+                          title: "You don't have any game saved yet",
+                          weight: FontWeight.normal,
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              } else {
                 return ListView(
                   physics: const BouncingScrollPhysics(),
                   children: snapshot.data!.docs
@@ -43,31 +72,12 @@ class _FavouritesPageState extends State<FavouritesPage> {
                       .toList(),
                 );
               }
-              return Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    LottieBuilder.network(
-                        'https://lottie.host/85b0686f-fad8-4930-a12f-704af412d345/AG04t7ZsMf.json'),
-                    const SizedBox(height: 10),
-                    const MyText(
-                        googleFont: GoogleFonts.lato,
-                        color: Colors.white,
-                        fontSize: 32,
-                        title: 'Ohh nooo!',
-                        weight: FontWeight.bold),
-                    const SizedBox(height: 10),
-                    const MyText(
-                        googleFont: GoogleFonts.lato,
-                        color: Colors.grey,
-                        fontSize: 20,
-                        title: "You don't have any game saved yet",
-                        weight: FontWeight.normal)
-                  ],
-                ),
-              );
-            }),
+            }
+            return const Center(
+              child: Text("Error loading favorites"),
+            );
+          },
+        ),
       ),
     );
   }
